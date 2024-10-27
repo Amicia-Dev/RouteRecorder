5067d5d3dc45ce5e1fc8240ac5e856bc6a2feca5
Amicia <143362445+Amicia-Dev@users.noreply.github.com>
2024-10-27 02:48:48 +0100

    Added sources for 1.21_1.21.1_1.21.2_1.21.3

---
+++ src_1.2_1.21.3/main/java/org/amicia/routerecorder/Routerecorder.java
@@ -0,0 +1,10 @@
+package org.amicia.routerecorder;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Routerecorder implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
